701a5dc5994add3dbd43b2c7c69505ac4c38c98c
RomanMitaki <r.mitaki@yandex.ru>
2022-11-16 22:33:44 +0300

    fix: added TS to utils folder

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,18 +1,52 @@
-import PropTypes from 'prop-types';
+export type TIngredient = {
+    readonly _id: string,
+    readonly name: string,
+    readonly type: string,
+    readonly proteins: number,
+    readonly fat: number,
+    readonly carbohydrates: number,
+    readonly calories: number,
+    readonly price: number,
+    readonly image: string,
+    readonly image_mobile: string,
+    readonly image_large: string,
+    readonly __v: number,
+};
 
-const ingredientType = PropTypes.shape({
-  _id: PropTypes.string,
-  name: PropTypes.string,
-  type: PropTypes.string,
-  proteins: PropTypes.number,
-  fat: PropTypes.number,
-  carbohydrates: PropTypes.number,
-  calories: PropTypes.number,
-  price: PropTypes.number,
-  image: PropTypes.string,
-  image_mobile: PropTypes.string,
-  image_large: PropTypes.string,
-  __v: PropTypes.number,
-});
+export type TOrder = {
+    readonly createdAt: string,
+    readonly ingredients: string[],
+    readonly name: string,
+    readonly number: null | number,
+    readonly price: number,
+    readonly status: string,
+    readonly updatedAt: string,
+    readonly _id: string,
+}
 
-export default ingredientType;
+export type TUser = {
+    name: string,
+    email: string,
+    password: string,
+};
+
+export type TLoginData = {
+    email: string,
+    password: string,
+};
+
+export type TResetPasswordData = {
+    token: string,
+    password: string,
+};
+
+export type TRegisterData = {
+    name: string,
+    email: string,
+    password: string,
+};
+
+export type TIngredientListResponse = {
+    data: ReadonlyArray<TIngredient>,
+    success: boolean,
+}
